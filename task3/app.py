11e23f6f2004f9bfbba09cec2c07e83e2ef6e785
Lauraparra <laura.parra28@gmail.com>
2023-03-30 18:28:58 -0500

    update

---
+++ task3/app.py
@@ -0,0 +1,132 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import helper_function as hf
+from info_data import films, new_films
+import os
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+list_files = []
+
+@app.route("/")
+def home():
+    return "Bem-vindo ao ICA"
+
+# CREATE A FILE 
+@app.route("/create_file",methods=["POST"])
+def create_file():
+    response = request.get_json()
+    filename = response.get("filename")
+    list_files.append(response)
+    
+    with open(filename,"w") as f:
+        f.write(f"O arquivo criado foi {filename}")
+    f.close()
+    return jsonify(list_files)
+
+# READ A FILE .txt
+@app.route("/read_file",methods=["POST"])
+def read_file():
+    response = request.get_json()
+    filename = response.get("filename")
+    print(filename)
+   
+    with open(filename,"r") as f:
+        conteudo = f.readlines()
+    f.close()
+    return jsonify(conteudo)
+
+# EDIT A FILE .txt
+@app.route("/edit_file",methods=["POST"])
+def edit_file():
+    response = request.get_json()
+    filename = response.get("filename")
+    data = response.get("message")
+    
+    with open(filename,"a") as f:
+        f.write(f" {data}")
+    f.close()
+    with open(filename, "r") as f:
+        conteudo = f.readlines()
+    f.close()    
+    return jsonify(
+        mensagem = "Arquivo editado",
+        data = conteudo
+    )
+
+# CREATE A .csv
+@app.route("/create_csv",methods=["POST"])
+def create_csv():
+    response = request.get_json()
+    filename = response.get("filename")
+    list_files.append(response)
+    
+    data_films = pd.DataFrame(films)
+    data_films.to_csv(filename, index=False)
+    
+    return jsonify(list_files)
+
+# EDIT A .create_csv
+
+@app.route("/edit_csv",methods=["POST"])
+def edit_csv():
+    response = request.get_json()
+    filename = response.get("filename")
+    message = response.get("message")
+    
+    data_films = pd.DataFrame(message)
+    data_films.to_csv(filename, index=False, header=False,mode="a")
+        
+    return jsonify(
+        mensagem = "Arquivo editado",
+        data = message
+    )
+    
+#  FILTRO para ler de n a m linhas
+
+@app.route("/filter_range",methods=["POST"])
+def filter_range():
+   
+    post_res = request.get_json()
+    filename = post_res.get("filename")
+    filter = post_res.get("filter")
+  
+    num_n = filter.get("num_n")
+    num_m = filter.get("num_m")
+    contents = hf.filter_range_csv(filename, num_n, num_m)
+    
+    return contents
+    
+#  FILTRO por coluna definida e valor desejado 'x'
+
+@app.route("/filter_data",methods=["POST"])
+def filter_data():
+   
+    post_res = request.get_json()
+    filename = post_res.get("filename")
+    filter = post_res.get("filter")
+  
+    column = filter.get("column")
+    value = filter.get("value")
+    contents = hf.filter_csv(filename, column, value)
+    
+    return contents  
+
+# FILTRO de filmes por categoria 'Genre': SciFi
+
+@app.route("/filter_genre",methods=["POST"])
+def filter_genre():
+    post_res = request.get_json()
+    filename = post_res.get("filename")
+    filter = post_res.get("filter")
+  
+    column = filter.get("column")
+    tipo = filter.get("tipo")
+    contents = hf.func_genre(filename, column, tipo)
+    
+    return contents  
+    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
